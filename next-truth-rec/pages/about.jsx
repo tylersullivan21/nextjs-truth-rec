969eae053d69c22f74a8472c1685b19efedfa302
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-03-19 12:19:17 -0400

    added content to about and events page

--- next-truth-rec/pages/about.jsx
+++ next-truth-rec/pages/about.jsx
@@ -10,6 +10,61 @@ render(){
 <div>
     <Navigation/>
     <Modal/>
+
+    <section class="main-about">
+    <h1 class="title-about">Alachua County Truth and Reconciliation Initiative.</h1>
+    <div class="about-content">
+         <p>Concerning the Truth and Reconciliation process, the Board passed the following:</p>
+
+         <ol class="motions">
+             <li>The County Commission hereby initiates its Truth and Reconciliation process, 
+                 the goal of which is to set an example for how local government can recall its role in our history of racial injustice, 
+                 and repair what it can through official apologies and appropriate reparations.</li>
+
+             <li>The County will research its history of voter suppression, segregation in housing and public accommodations, discrimination in hiring and economic opportunity, 
+                 participation in leased prison labor to fund county government and build/maintain public facilities, differences in the provision of government services, 
+                 and any other government actions or inactions that deserve to be remembered.
+            </li>
+
+             <li>The County will cooperate in and co-sponsor one or more symposiums or conferences where “truth and reconciliation” is the topic, 
+                 and where experts with experience in similar processes are invited to provide insights.
+            </li>
+
+             <li>The County will facilitate town hall meetings where people can tell their story and react to historical findings.</li>
+
+             <li>The County will co-sponsor and assist in one or more public reading projects, such as the Gainesville Mayor’s Book Club. </li>
+
+             <li>The County will create an online archive of documents, images and recordings, related to this project. 
+                 This may be done in cooperation with the County’s constitutional officers, the Library District, the University of Florida, and local historic museums. 
+                 As much as possible, original documents and interviews with those directly affected will be collected rather than relying on secondhand accounts and historical interpretations. 
+                 The County website will provide the County’s role but should provide hyperlinks to the other resources from the other entities participating in the process.</li>
+
+             <li>By letter from the Chair, other public and private institutions will be invited to participate in a similar process for their organization, 
+                 with a goal to complete the research phase by 2020.</li>
+
+             <li>The County will formally request that the memorial slab of Alachua County victims of lynching be transferred to County government: 
+                 <ol class="a">
+                 <li>As soon as the EJI requirements have been met for this truth and reconciliation process</li>
+                <li>  Determined through a public process where and how the monument will be displayed </li>
+                <li>  Have raised sufficient public and private funds required to properly display and interpret the monument. </li>
+                <li>  Explore how those names not on the EJI Memorial will be properly memorialized in Alachua County and Montgomery Alabama. </li>
+                <li>  Explore the possibility of collecting jars of soil from or near the lynching sites as well as providing some to the display in Montgomery Alabama. </li>
+                <li>  Explore the possibility of road signs near the sites that would direct those interested in the matter to the information. </li>
+                </ol>
+             </li>
+             <li>This motion will be provided as a referral to all County advisory boards, with a request for them to provide a short-written recommendation for the scope of their participation, 
+                 any areas of inquiry that they believe should be included, or any proposed revisions to this process.
+            </li>
+
+             <li>The County Manager an Attorney are requested to return with their analysis of the public and private resources needed to complete the tasks suggested in this motion, 
+                 and any further revisions including legal liabilities, for further consideration by the Board of County Commissioners as an item on a regular agenda during an evening meeting. 
+                 The County Manager will discuss with the University of Florida their willingness to participate in the process and any cost associated with their efforts. 
+                 The County Manager shall explore how to send County employees on a trip to view the EGI Museum in Alabama and report back to the Board with how that would work. 
+                 There should be an interim report in January informing the Board of the Manager’s efforts and suggestions.
+            </li>
+         </ol>
+    </div>
+</section>
     <Footer/>
 </div>
 )
--- next-truth-rec/pages/events.jsx
+++ next-truth-rec/pages/events.jsx
@@ -10,6 +10,89 @@ render(){
 <div>
     <Navigation/>
     <Modal/>
+    <section id="events-main" class="main">
+        <div id="main-content">
+        <h1 class="title">Upcoming Events </h1>
+            <div>
+              <h3><a href="https://afam.clas.ufl.edu/african-american-studies-symposium/"><strong>2/20/2020 -
+                                    African American Studies Turns 50! Symposium &amp; Anniversary Celebration</strong></a></h3>
+                  <p>This Anniversary program is free and open to the public. Please RSVP by @
+                            africanamericanstudies.eventbrite.com</p><br/>
+                  <a href="https://www.eventbrite.com/e/african-american-studies-50th-celebration-uf-tickets-79771160689">Click here to register &gt;</a>                
+            </div>
+            <hr/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <h2 class="title">Past Events</h2>
+              <div>
+                <h3><strong>2/17/2020 - Alachua County BoCC Special Meeting/Workshop – Truth and Reconciliation Community Remembrance Project</strong></h3>
+                <p>Alachua County is hosting a community workshop to discuss plans for commemorating and recognizing victims of lynching with plans to erect historical markers throughout the county, which will create memorials that acknowledges the horrors of racial injustice. <strong>This workshop will be held at the Senior Recreation Center, 5701 NW 34th Blvd., Gainesville, FL 32653, at 6:00 p.m. It is open to the public.</strong></p><br/>
+              </div>
+              <hr/>
+                    <div>
+                        <h3><a href="https://www.hailehomestead.org/"><strong>2/15/2020 - Reclaiming Kin: Once Lost, Now Found</strong></a></h3>
+                        <p>Beginning February 15th at 10 am at the Allen &amp; Ethel Graham Visitors Center and Museum at the Historic Haile Homestead, the newest exhibit "Reclaiming Kin: Once Lost, Now Found," opens to the public for the first time! On Feb. 15 Museum is free. Tours of the Homestead will be conducted at 10:15 am, 11:15 am, 12:15 pm and 1:15 pm for $5 per person, under 12 free.</p>
+                        <br/><a href="https://www.hailehomestead.org/">Learn More &gt;</a>
+                    </div>
+                    <hr/>
+                    <div>
+                        <h3><a href="https://rosewoodflorida.com/"><strong>2/13/2020 - Reclaiming Kin:  The Legacy of Hampton and Grace Hathcock</strong></a></h3>
+                        <p>Join us for a talk on Thursday, February 13th at 7 pm for "Reclaiming Kin: The Legacy of Hampton &amp; Grace Hathcock," by Tatanya Peterson, Hathcock descendant and genealogist, and Karen Kirkman, Historian at Kanapaha Presbyterian Church, 6221 SW 75th Terrace, Gainesville.</p>
+                        <p>Peterson will share how she traced her family line back to Hampton and Grace Hathcock, her 4X great grandparents, who were enslaved on the plantation of James Chesnut, near Alachua, Florida. Learn about the discoveries she and Kirkman made, and the resources they both used in their research. FREE and open to the public; refreshments following talk.</p>
+                        <br/><a href="https://rosewoodflorida.com/">Learn More &gt;</a>
+                    </div>
+                    <hr/>
+                    
+                    <div>
+                        <h3><a href="https://www.alachuacounty.us/news/Article/Pages/Memorial-Service-for-the-Victims-of-Lynching-in-Alachua-County.aspx"><strong>2/7/2020 - Memorial Service for the Victims of Lynching in Alachua County</strong></a></h3>
+                        <p>Alachua County is hosting a Memorial Service for the Victims of Lynching in Alachua County on
+                            Friday, February 7, 2020, beginning at 6 p.m. at Mount Pleasant United Methodist Church (630
+                            N.W. 2nd Street, Gainesville). The event is free and open to the public.</p><br/>
+                            <a href="https://www.alachuacounty.us/news/Article/Pages/Memorial-Service-for-the-Victims-of-Lynching-in-Alachua-County.aspx">Learn
+                                More &gt;</a>
+                    </div>
+                    <hr/>
+                    <div>
+                        <h3><strong>2/3/2020 - Truth &amp; Reconciliation Update and Presentation to joint meeting of City of Gainesville/County Commissions</strong></h3>
+                    </div>
+                    <hr/>
+                    <div>
+                        <h3><strong>1/28/2020 - Truth &amp; Reconciliation Update and Presentation to County Commission</strong></h3>
+                    </div>
+                    <hr/>
+                    <div>
+                        <h3><strong>1/1/2020 -  “Rosewood Day” Proclamation &amp; Candle Light Ceremony</strong></h3>
+                        <p>At Rosewood Historic Marker in adjoining Levy County; the history impacted Alachua County with survivors fleeing to Archer</p><br/><a href="https://rosewoodflorida.com/">Learn more &gt;</a>
+                    </div>
+                    <hr/>
+                    <div>
+                        <h3><strong>12/10/2019 - Presentation on the history of the Newberry Lynchings of 1916, by UF historian Dr. Tricia Hilliard Nunn.&nbsp; Concerned Citizens of Newberry</strong></h3>
+                    </div>
+                    <hr/>
+                    <div>
+                        <h3><strong>9/18/2019 - State Historic Marker Dedication, Newberry Lynchings of 1916 (text by Dr. Tricia Hilliard Nunn).&nbsp; At Pleasant Plain UMC, Newberry</strong></h3>
+                    </div>
+                    <hr/>
+                    <div>
+                        <h3><strong>3/21/2019 - The African American Oral History Symposium, 'From Segregation to Black Lives Matter.' UF Samuel Proctor Oral History Program</strong></h3>
+                    </div>
+                    <hr/>
+                    <div>
+                        <h3><strong>9/20/2018 - Reconciliation meeting of Newberry Lynching history. Concerned Citizens of Newberry</strong></h3>
+                    </div>
+                    <hr/>
+                    <div>
+                        <h3><strong>7/17/2018 - Discussion of Newberry Lynching history. Concerned Citizens of Newberry</strong></h3>
+                    </div>
+                    <hr/>
+                    <div>
+                        <h3><strong>10/24/2017 - 'An Outrage, Film viewing and Panel Discussion. Matheson History Museum</strong></h3>
+                    </div>
+                    <hr/>
+                </div>
+            </section>
     <Footer/>
 </div>
 )
