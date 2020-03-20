1b3fb2f75f1732061f78d4aa6f261b3fe815655b
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2020-03-20 15:40:13 -0400

    rendered and styled documents page

--- next-truth-rec/pages/events.jsx
+++ next-truth-rec/pages/events.jsx
@@ -1,6 +1,3 @@
-import Navigation from "../components/Navigation";
-import Modal from "../components/Modal";
-import Footer from "../components/Footer";
 import Layout from "../components/Layout";
 import eventsStyles from "../public/jsx-styles/events-styles";
 
