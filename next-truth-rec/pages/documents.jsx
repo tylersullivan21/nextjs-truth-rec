15ab1ec69d9406a86a5ead6374c1c0a77b1a5325
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-03-23 13:09:00 -0400

    fixed docs styling, next js styling doesnt work with id

--- next-truth-rec/pages/documents.jsx
+++ next-truth-rec/pages/documents.jsx
@@ -6,17 +6,17 @@ class DocumentsPage extends React.Component{
 render(){
     return(
 <Layout>
-<section id="documents-main" >
-            <section id="intro">
+<section className="documents-main" >
+            <section className="intro">
                 <h1>Documents</h1>
                     
             </section>
 
-            <section id="search">
+            <section className="search">
 
             </section>
 
-            <section id="minutes-title">
+            <section className="minutes-title">
                 <div>
                     <h1>Meeting Minutes</h1>
                 </div>
@@ -25,18 +25,18 @@ render(){
                 <div className="item">
                     <h3 className="item__headline">June 1, 1863 - Board of County Commission Minutes</h3>
             
-                    <p className="item__text">At a meeting of the board of County Commissioners, met pursuant to adjournment, Present Mathew Hinson, Enoch T. Geiger, J.M. Sparkman C. Rain &amp; E.H. Jordan President of the board. Ordered that J.R.W. Grissoms account of One dollar for altering Table in Judge of probates Office be paid.</p>
+                    <p className="item__text">At a meeting of the board of County Commissioners, met pursuant to adjournment, Present Mathew Hinson, Enoch T. Geiger, J.M. Sparkman C. Rain &amp; E.H. Jordan PresclassNameent of the board. Ordered that J.R.W. Grissoms account of One dollar for altering Table in Judge of probates Office be paclassName.</p>
             
-                    <a href="https://www.alachuaclerk.org/archive/Clerk/Ancient/FrontPage.cfm?DID=171&amp;BID=620&amp;Trans=171" target="blank" className="item__button">Learn More »</a>
+                    <a href="https://www.alachuaclerk.org/archive/Clerk/Ancient/FrontPage.cfm?DclassName=171&amp;BclassName=620&amp;Trans=171" target="blank" className="item__button">Learn More »</a>
                     <img className="item__image" src="img/doc.png" alt="Document"/>
                 </div>
             
                 <div className="item">
                     <h3 className="item__headline">May 7, 1866 - Board of County Commission Minutes</h3>
             
-                    <p className="item__text">At a meeting of the board of County Commissioners, met pursuant to adjournment, Present Mathew Hinson, Enoch T. Geiger, J.M. Sparkman C. Rain &amp; E.H. Jordan President of the board.</p>
+                    <p className="item__text">At a meeting of the board of County Commissioners, met pursuant to adjournment, Present Mathew Hinson, Enoch T. Geiger, J.M. Sparkman C. Rain &amp; E.H. Jordan PresclassNameent of the board.</p>
             
-                    <a href="http://www.alachuaclerk.org/archive/Clerk/Ancient/FrontPage.cfm?DID=202&amp;BID=620&amp;Trans=202" target="blank" className="item__button">Learn More »</a>
+                    <a href="http://www.alachuaclerk.org/archive/Clerk/Ancient/FrontPage.cfm?DclassName=202&amp;BclassName=620&amp;Trans=202" target="blank" className="item__button">Learn More »</a>
                     
                     <img className="item__image" src="img/doc.png" alt="Document"/>
                 </div>
--- next-truth-rec/public/jsx-styles/documents-styles.js
+++ next-truth-rec/public/jsx-styles/documents-styles.js
@@ -1,187 +1,188 @@
 import css from 'styled-jsx/css'
 
 export default css `
-#documents-main{
-    width: 90vw;
-    /*min-height: calc(100vh - 180px);*/
-    display:-ms-grid;
-    display:grid;
-    -ms-grid-columns: 15% 1fr 15%;
-    grid-template-columns: 15% 1fr 15%;
-    -ms-grid-rows: -webkit-min-content 0 -webkit-min-content -webkit-max-content;
-    -ms-grid-rows: min-content 0 min-content max-content;
-    grid-template-rows: -webkit-min-content 0 -webkit-min-content -webkit-max-content;
-    grid-template-rows: min-content 0 min-content max-content;
-        grid-template-areas: 
-    ". intro ." 
-    ". search ."
-    ". minutes-title ."
-    ". items .";
+.documents-main{
+  width: 90vw;
+  /*min-height: calc(100vh - 180px);*/
+  display:-ms-grid;
+  display:grid;
+  -ms-grid-columns: 15% 1fr 15%;
+  grid-template-columns: 15% 1fr 15%;
+  -ms-grid-rows: -webkit-min-content 0 -webkit-min-content -webkit-max-content;
+  -ms-grid-rows: min-content 0 min-content max-content;
+  grid-template-rows: -webkit-min-content 0 -webkit-min-content -webkit-max-content;
+  grid-template-rows: min-content 0 min-content max-content;
+      grid-template-areas: 
+  ". intro ." 
+  ". search ."
+  ". minutes-title ."
+  ". items .";
+}
+.intro{
+    -ms-grid-row:1;
+    -ms-grid-column:2;
+    grid-area:intro;
+}
+.search {
+    -ms-grid-row: 2;
+    -ms-grid-column: 2;
+    grid-area: search;
+}
+.minutes-title{
+    -ms-grid-row: 3;
+    -ms-grid-column: 2;
+    grid-area: minutes-title;
+}
+#card-container{
+   /* grid-column: 2/3;*/
+}
+.card{
+    display: -ms-grid;
+    display: grid;
+    -ms-grid-columns: 30% 1fr;
+    grid-template-columns: 30% 1fr;
+}
+
+.doc1-img1{
+    -ms-grid-column-align: center;
+        justify-self: center;
+}
+
+/*1024 AND LARGER STYLES END HERE*/
+
+
+
+
+
+
+/* CODE FOR MAIN CONTENT AREA STARTS HERE*/
+
+.items {
+    -ms-grid-row:4;
+    -ms-grid-column:2;
+    /*grid-column: 2/3;*/
+    grid-area:items;
+    /*display:flex;*/
+}
+.item {
+    background-color: #001621;
+    color: white;
+    display: -ms-grid;
+    display: grid;
+    -ms-grid-columns: 1fr ;
+    grid-template-columns: 1fr ;
+    /*grid-template-rows:  1fr auto auto auto 1fr ;*/
+        grid-template-areas: "." "headline" "dtext" "dbutton" ".";
+    margin: 2rem auto;
+    text-align: center;
+  
   }
-  #intro{
-      -ms-grid-row:1;
-      -ms-grid-column:2;
-      grid-area:intro;
+  .item__image {
+    grid-area: image;
+    z-index: 0;
+    -ms-grid-row-align: stretch;
+        align-self: stretch;
+    -o-object-fit: cover;
+       object-fit: cover;
+    mix-blend-mode: overlay;
+    max-width: 100%;
+    display: none;
   }
-  #search {
-      -ms-grid-row: 2;
-      -ms-grid-column: 2;
-      grid-area: search;
+  .item__headline {
+    -ms-grid-row: 2;
+    -ms-grid-column: 1;
+    grid-area: headline;
+    z-index: 1;
+    margin-left: 1rem;
+    margin-right: 1rem;
+    -ms-grid-row-align: end;
+        align-self: end;
   }
-  #minutes-title{
-      -ms-grid-row: 3;
-      -ms-grid-column: 2;
-      grid-area: minutes-title;
+  .item__text {
+    -ms-grid-row: 3;
+    -ms-grid-column: 1;
+    grid-area: dtext;
+    z-index: 1;
+    margin-left: 1rem;
+    margin-right: 1rem;
   }
-  #card-container{
-     /* grid-column: 2/3;*/
-  }
-  #card{
-      display: -ms-grid;
-      display: grid;
-      -ms-grid-columns: 30% 1fr;
-      grid-template-columns: 30% 1fr;
-  }
-  
-  .doc1-img1{
-      -ms-grid-column-align: center;
-          justify-self: center;
+  .item__button {
+    -ms-grid-row: 4;
+    -ms-grid-column: 1;
+    grid-area: dbutton;
+    margin:1em;
+    z-index: 1;
+    display:block;
   }
   
-  /*1024 AND LARGER STYLES END HERE*/
-  
-  
-  
-  
-  
-  
-  /* CODE FOR MAIN CONTENT AREA STARTS HERE*/
-  
-  .items {
-      -ms-grid-row:4;
-      -ms-grid-column:2;
-      /*grid-column: 2/3;*/
-      grid-area:items;
-      /*display:flex;*/
+  .item__headline {
+    font-size: 1.5rem;
+    margin-bottom: .5em;
   }
-  .item {
-      background-color: #001621;
-      color: white;
-      display: -ms-grid;
-      display: grid;
-      -ms-grid-columns: 1fr ;
-      grid-template-columns: 1fr ;
-      /*grid-template-rows:  1fr auto auto auto 1fr ;*/
-          grid-template-areas: "." "headline" "dtext" "dbutton" ".";
-      margin: 2rem auto;
-      text-align: center;
-    
+  
+  .item__button {
+    color: #efbb53;
+    font-size: 1rem;
+  }
+
+  @media (min-width: 640px) and (max-width: 1023px) {
+    .items {
+    /*  display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: 1rem; */
     }
-    .item__image {
-      grid-area: image;
-      z-index: 0;
-      -ms-grid-row-align: stretch;
-          align-self: stretch;
-      -o-object-fit: cover;
-         object-fit: cover;
-      mix-blend-mode: overlay;
-      max-width: 100%;
-      display: none;
+  }
+
+  @media (min-width: 1024px) {
+    .documents-main{
+        -ms-grid-columns: 22% 1fr 22%;
+        grid-template-columns: 22% 1fr 22%;
+    }
+    .items {
+    /*  grid-column: 2/3;*/
+    }
+    .item {
+      background-color: transparent;
+      color: black;
+      -ms-grid-columns: 1fr 20px 5fr;
+      grid-template-columns: 1fr 5fr;
+          grid-template-areas: "image ." "image headline" "image dtext" "image dbutton" "image .";
+      grid-column-gap: 20px;
+      text-align: left;
     }
     .item__headline {
-      -ms-grid-row: 2;
-      -ms-grid-column: 1;
-      grid-area: headline;
-      z-index: 1;
-      margin-left: 1rem;
-      margin-right: 1rem;
-      -ms-grid-row-align: end;
-          align-self: end;
-    }
-    .item__text {
-      -ms-grid-row: 3;
-      -ms-grid-column: 1;
-      grid-area: dtext;
-      z-index: 1;
-      margin-left: 1rem;
-      margin-right: 1rem;
-    }
-    .item__button {
-      -ms-grid-row: 4;
-      -ms-grid-column: 1;
-      grid-area: dbutton;
-      margin:1em;
-      z-index: 1;
-      display:block;
-    }
-    
-    .item__headline {
-      font-size: 1.5rem;
+      font-size: 1rem;
       margin-bottom: .5em;
     }
-    
+
+    .item__text {
+      margin: 0;
+    }
+  /*  .item.even {
+      grid-template-areas: "image ." "image headline" "image dtext" "image dbutton" "image .";
+    }*/
+    .item__image {
+      display:block;
+      -o-object-fit: contain;
+         object-fit: contain;
+    }
+    .item__image {
+    -ms-grid-row: 1;
+    -ms-grid-row-span: 5;
+    -ms-grid-column: 1;
+    }
+    .item__headline {
+    -ms-grid-row: 2;
+    -ms-grid-column: 3;
+    }
+    .item__text {
+    -ms-grid-row: 3;
+    -ms-grid-column: 3;
+    }
     .item__button {
-      color: #efbb53;
-      font-size: 1rem;
+    -ms-grid-row: 4;
+    -ms-grid-column: 3;
     }
-  
-    @media (min-width: 640px) and (max-width: 1023px) {
-      .items {
-      /*  display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-gap: 1rem; */
-      }
-    }
-  
-    @media (min-width: 1024px) {
-      #main-section {
-          -ms-grid-columns: 22% 1fr 22%;
-          grid-template-columns: 22% 1fr 22%;
-      }
-      .items {
-      /*  grid-column: 2/3;*/
-      }
-      .item {
-        background-color: transparent;
-        color: black;
-        -ms-grid-columns: 1fr 20px 5fr;
-        grid-template-columns: 1fr 5fr;
-            grid-template-areas: "image ." "image headline" "image dtext" "image dbutton" "image .";
-        grid-column-gap: 20px;
-        text-align: left;
-      }
-      .item__headline {
-        font-size: 1rem;
-        margin-bottom: .5em;
-      }
-  
-      .item__text {
-        margin: 0;
-      }
-    /*  .item.even {
-        grid-template-areas: "image ." "image headline" "image dtext" "image dbutton" "image .";
-      }*/
-      .item__image {
-        display:block;
-        -o-object-fit: contain;
-           object-fit: contain;
-      }
-      .item__image {
-      -ms-grid-row: 1;
-      -ms-grid-row-span: 5;
-      -ms-grid-column: 1;
-      }
-      .item__headline {
-      -ms-grid-row: 2;
-      -ms-grid-column: 3;
-      }
-      .item__text {
-      -ms-grid-row: 3;
-      -ms-grid-column: 3;
-      }
-      .item__button {
-      -ms-grid-row: 4;
-      -ms-grid-column: 3;
-      }
-    }
+  }
+
 `
