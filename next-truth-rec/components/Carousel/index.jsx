884e503098602b88febc70288052cd9f459728c1
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-03-31 10:18:17 -0400

    added Carousel component, installed jquery, flickity

---
+++ next-truth-rec/components/Carousel/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import $ from "jquery";
+
+class Carousel extends React.Component{
+
+    /*ADD JQUERY HERE 
+componentDidMount(){
+
+}
+*/
+}
+
+export default Carousel;
