0f19e929178ff7db6531b07300b9195f567b09a0
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2020-04-03 15:50:29 -0400

    created unique video page, added common components

---
+++ next-truth-rec/pages/videopage/index.jsx
@@ -0,0 +1,22 @@
+
+import Layout from "../../components/Layout";
+
+class VideoPage extends React.Component {
+
+render(){
+
+return(
+
+<Layout>
+
+
+</Layout>
+)
+
+}
+
+
+
+}
+
+export default VideoPage
