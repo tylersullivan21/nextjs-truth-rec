7863ca9776dec76b92d80f0f5b1178f4090fb14c
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-04-07 14:15:43 -0400

    working on grid for video page

--- next-truth-rec/pages/videopage/index.jsx
+++ next-truth-rec/pages/videopage/index.jsx
@@ -1,6 +1,6 @@
 
 import Layout from "../../components/Layout";
-
+import videoStyles from "./video-styles";
 class VideoPage extends React.Component {
 
 render(){
@@ -9,7 +9,20 @@ return(
 
 <Layout>
 
+<div className="container">
+    <div className="video">
 
+    </div>
+
+    <div className="about-video">
+
+    </div>
+
+    <div className="related">
+
+    </div>
+</div>
+<style jsx>{videoStyles}</style>
 </Layout>
 )
 
